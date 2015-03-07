80aa7b1c9eab3b53ef831aca93f17cb3ff774244
Moritz Loos <moritz.loos@uni-weimar.de>
2015-03-07 18:25:44 +0100

    correct triangulation. now all triangulations seems to be correct, because they give the same results for points (up to scale).

--- motionEstimation/Triangulation.cpp
+++ motionEstimation/Triangulation.cpp
@@ -31,9 +31,13 @@ cv::Mat_<double> IterativeLinearLSTriangulation(cv::Point3d point2d1_h,         
                                                 ) {
     double wi = 1, wi1 = 1;
     cv::Mat_<double> X(4,1);
-    cv::Mat_<double> X_ = LinearLSTriangulation(point2d1_h,P0,point2d2_h,P1);
-    X(0) = X_(0); X(1) = X_(1); X(2) = X_(2); X(3) = 1.0;
     for (int i=0; i<10; i++) { //Hartley suggests 10 iterations at most
+        cv::Mat_<double> X_ = LinearLSTriangulation(point2d1_h,P0,point2d2_h,P1);
+        X(0) = X_(0);
+        X(1) = X_(1);
+        X(2) = X_(2);
+        X(3) = 1.0;
+
         //recalculate weights
         double p2x = cv::Mat_<double>(cv::Mat_<double>(P0).row(2)*X)(0);
         double p2x1 = cv::Mat_<double>(cv::Mat_<double>(P1).row(2)*X)(0);
@@ -45,19 +49,25 @@ cv::Mat_<double> IterativeLinearLSTriangulation(cv::Point3d point2d1_h,         
         wi1 = p2x1;
 
         //reweight equations and solve
-        cv::Matx43d A((point2d1_h.x*P0(2,0)-P0(0,0))/wi,		(point2d1_h.x*P0(2,1)-P0(0,1))/wi,			(point2d1_h.x*P0(2,2)-P0(0,2))/wi,
-                      (point2d1_h.y*P0(2,0)-P0(1,0))/wi,		(point2d1_h.y*P0(2,1)-P0(1,1))/wi,			(point2d1_h.y*P0(2,2)-P0(1,2))/wi,
-                      (point2d2_h.x*P1(2,0)-P1(0,0))/wi1,	(point2d2_h.x*P1(2,1)-P1(0,1))/wi1,		(point2d2_h.x*P1(2,2)-P1(0,2))/wi1,
-                      (point2d2_h.y*P1(2,0)-P1(1,0))/wi1,	(point2d2_h.y*P1(2,1)-P1(1,1))/wi1,		(point2d2_h.y*P1(2,2)-P1(1,2))/wi1
-                      );
-        cv::Mat_<double> B = (cv::Mat_<double>(4,1) <<	  -(point2d1_h.x*P0(2,3)	-P0(0,3))/wi,
+        cv::Matx43d A(
+                    (point2d1_h.x*P0(2,0)-P0(0,0))/wi,	(point2d1_h.x*P0(2,1)-P0(0,1))/wi,	(point2d1_h.x*P0(2,2)-P0(0,2))/wi,
+                    (point2d1_h.y*P0(2,0)-P0(1,0))/wi,	(point2d1_h.y*P0(2,1)-P0(1,1))/wi,	(point2d1_h.y*P0(2,2)-P0(1,2))/wi,
+                    (point2d2_h.x*P1(2,0)-P1(0,0))/wi1,	(point2d2_h.x*P1(2,1)-P1(0,1))/wi1,	(point2d2_h.x*P1(2,2)-P1(0,2))/wi1,
+                    (point2d2_h.y*P1(2,0)-P1(1,0))/wi1,	(point2d2_h.y*P1(2,1)-P1(1,1))/wi1,	(point2d2_h.y*P1(2,2)-P1(1,2))/wi1
+                    );
+
+        cv::Mat_<double> B = (cv::Mat_<double>(4,1) <<
+                              -(point2d1_h.x*P0(2,3)	-P0(0,3))/wi,
                               -(point2d1_h.y*P0(2,3)	-P0(1,3))/wi,
                               -(point2d2_h.x*P1(2,3)	-P1(0,3))/wi1,
                               -(point2d2_h.y*P1(2,3)	-P1(1,3))/wi1
                               );
 
         cv::solve(A,B,X_,cv::DECOMP_SVD);
-        X(0) = X_(0); X(1) = X_(1); X(2) = X_(2); X(3) = 1.0;
+        X(0) = X_(0);
+        X(1) = X_(1);
+        X(2) = X_(2);
+        X(3) = 1.0;
     }
     return X;
 }
@@ -72,30 +82,29 @@ cv::Mat_<double> LinearLSTriangulation(
         cv::Matx34d P1//camera 2 matrix
         )
 {
-    //build A matrix
-    cv::Matx43d A(u.x*P(2,0)-P(0,0),u.x*P(2,1)-P(0,1),u.x*P(2,2)-P(0,2),
-              u.y*P(2,0)-P(1,0),u.y*P(2,1)-P(1,1),u.y*P(2,2)-P(1,2),
-              u1.x*P1(2,0)-P1(0,0), u1.x*P1(2,1)-P1(0,1),u1.x*P1(2,2)-P1(0,2),
-              u1.y*P1(2,0)-P1(1,0), u1.y*P1(2,1)-P1(1,1),u1.y*P1(2,2)-P1(1,2)
-              );
+    //build matrix A for homogenous equation system Ax = 0
+    //assume X = (x,y,z,1), for Linear-LS method
+    //which turns it into a AX = B system, where A is 4x3, X is 3x1 and B is 4x1
+    cv::Matx43d A(
+        u.x*P(2,0)-P(0,0),    u.x*P(2,1)-P(0,1),      u.x*P(2,2)-P(0,2),
+        u.y*P(2,0)-P(1,0),    u.y*P(2,1)-P(1,1),      u.y*P(2,2)-P(1,2),
+        u1.x*P1(2,0)-P1(0,0), u1.x*P1(2,1)-P1(0,1),   u1.x*P1(2,2)-P1(0,2),
+        u1.y*P1(2,0)-P1(1,0), u1.y*P1(2,1)-P1(1,1),   u1.y*P1(2,2)-P1(1,2)
+    );
+
     //build B vector
-    cv::Matx41d B(-(u.x*P(2,3)-P(0,3)),
-              -(u.y*P(2,3)-P(1,3)),
-              -(u1.x*P1(2,3)-P1(0,3)),
-              -(u1.y*P1(2,3)-P1(1,3)));
+    cv::Mat_<double> B = (cv::Mat_<double>(4,1) <<
+                  -(u.x*P(2,3)      -P(0,3)),
+                  -(u.y*P(2,3)      -P(1,3)),
+                  -(u1.x*P1(2,3)    -P1(0,3)),
+                  -(u1.y*P1(2,3)    -P1(1,3))
+    );
 
     //solve for X
     cv::Mat_<double> X;
     cv::solve(A,B,X,cv::DECOMP_SVD);
 
-    // convert to homogenious 3D point
-    cv::Mat_<double> XX(4, 1);
-    XX(0) = X(0);
-    XX(1) = X(1);
-    XX(2) = X(2);
-    XX(3) = 1.0;
-
-    return XX;
+    return X;
 }
 
 //http://pastebin.com/UE6YW39J
@@ -107,22 +116,22 @@ void TriangulatePointsHZ(
         const cv::Mat& Kinv,
         vector<cv::Point3f>& pointcloud)
 {
+    pointcloud.clear();
     for (unsigned int i=0; i < points1.size(); i++) {
-        //convert to normalized homogeneous coordinates
-        cv::Point3f point1_h(points1[i].x, points1[i].y, 1.0);
-        cv::Mat_<double> um = Kinv * cv::Mat_<double>(point1_h);
-        point1_h = um.at<cv::Point3f>(0);
+        cv::Point2f kp = points1[i];
+        cv::Point3f u (kp.x, kp.y, 1.0);
 
-        cv::Point3f point2_h(points2[i].x, points2[i].y, 1.0);
-        cv::Mat_<double> um1 = Kinv * cv::Mat_<double>(point2_h);
-        point2_h = um1.at<cv::Point3f>(0);
+        cv::Mat_<double> um = Kinv * cv::Mat_<double>(u);
+        u.x = um(0); u.y = um(1); u.z = um(2);
 
-        //triangulate
-        cv::Mat_<double> X = LinearLSTriangulation(point1_h, P0, point2_h, P1);
-        //cv::Mat_<double> X = IterativeLinearLSTriangulation(point1_h, P0, point2_h, P1);
+        cv::Point2f kp1 = points2[i];
+        cv::Point3d u1(kp1.x,kp1.y,1.0);
+        cv::Mat_<double> um1 = Kinv * cv::Mat_<double>(u1);
+        u1.x = um1(0); u1.y = um1(1); u1.z = um1(2);
 
-        //store 3D point
-        pointcloud.push_back(cv::Point3f(X(0),X(1),X(2)));
+        cv::Mat_<double> X = IterativeLinearLSTriangulation(u,P0,u1,P1);
+
+        pointcloud.push_back(cv::Point3d(X(0),X(1),X(2)));
     }
 }
 
